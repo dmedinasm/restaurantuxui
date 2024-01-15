1f324b4e0a99468e16d8843278010d0b995c66a9
dmedinas <dmedinas@gmail.com>
2024-01-15 17:38:00 +0100

    Creados archivos iniciales del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold underline text-blue-500">
+        Hello World !
+      </h1>
+    </>
+  )
+}
+
+export default App
