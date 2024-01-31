2b299d09f89f1204f020d7ea643a897a68f90540
dmedinas <dmedinas@gmail.com>
2024-01-31 18:31:23 +0100

    Creada seccion Blogs

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
-const Article = () => {
+import imageblog from '../../assets/img1_blog.png'
+const Article = ({text, date, author,title, image}) => {
   return (
-    <div>Article</div>
+    <article className="flex flex-col  justify-center "> 
+      <img src={image} alt="image card" className="max-w-[100%]  " />
+      <div className="flex flex-row justify-between items-center mt-2 mb-12">
+          <p className="font-opensans font-normal text-[16px] text-[#fafafa] leading-[175%] tracking-[4%]">{date}</p>
+          <p className="font-opensans font-normal text-[16px] text-[#fafafa] leading-[175%] tracking-[4%]">{author}</p>
+        </div>
+      <div className="flex flex-col  justify-center gap-8 max-h-[100%] h-[300px] ">
+        <h2 className="font-cormorant font-normal text-[2rem] hover:underline cursor-pointer text-primary leading-[130%] tracking-[4%]">{title}</h2>
+        <p className="font-opensans font-normal text-[16px] text-colortext leading-[175%] tracking-[4%]">{text}</p>
+        <p className="font-bold font-cormorant text-[1rem] cursor-pointer text-primary leading-[175%] tracking-[4%]">Read More</p>
+      </div>
+    </article>
   )
 }
 
--- src/containers/Blog/Blog.jsx
+++ src/containers/Blog/Blog.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
-
+import Article from '../../components/Article/Article'
+import decoration from '../../assets/decorationTitle.png'
+import img1 from '../../assets/img1_blog.png'
+import img2 from '../../assets/img2_blog.png'
+import img3 from '../../assets/img3_blog.png'
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section className="flex flex-row items-center justify-center py-[200px] px-[150px]">
+      <article className="flex flex-col items-center justify-center gap-12">
+        <div className="flex flex-col items-center justify-center">
+          <h3 className="font-cormorant font-bold text-[23px] text-white leading-[130%] tracking-[4%]">Blogs</h3>
+          <img src={decoration} alt='decoration title' />
+          <h1 className="font-semibold font-cormorant text-[64px] text-center md:text-start text-primary leading-[130%] tracking-[3%]">Gerícht Updates</h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-y-[64px] lg:grid-cols-3 gap-8">
+          <Article 
+          title={"Tips For Prepping And Caring For Your Grill"} 
+          text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in."}
+          image={img1}
+          author = {"- Annalisa L"}
+          date={"16 Apr 2021"}
+          />
+          <Article 
+          title={"Summer Cocktails And Mocktails"} 
+          text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in."}
+          image={img2}
+          author = {"- John Micheal"}
+          date={"23 May 2021"}
+          />
+          <Article 
+          title={"Easy Cooking For College Students"} 
+          text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in."}
+          image={img3}
+          author = {"- Fred W"}
+          date={"06 Aug 2021"}
+          />
+         
+        </div>
+        <button className="bg-primary py-2 px-8">
+          <p className="font-bold font-cormorant text-[1rem] text-[#0C0C0C] leading-[175%] tracking-[4%]">View More</p>
+        </button>
+      </article>
+    </section>
   )
 }
 
